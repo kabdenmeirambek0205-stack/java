529bce3fc9cc7e26a973fb4bc353984c9d2de38d
kabdenmeirambek0205 <kabdenmeirambek0205@gmail.com>
2026-02-09 12:10:57 +0500

    lab1

---
+++ lab1/new-Clock/main.java
@@ -0,0 +1,7 @@
+import java.time.LocalTime;
+public class main {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.now();
+        System.out.println("Current time: " + time);
+    }
+}
